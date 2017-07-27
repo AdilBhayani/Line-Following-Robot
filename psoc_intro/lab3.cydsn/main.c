36afe380670a2b10041bd021ebe6c96c3ba02e42
சகாயன் சிற்சபேசன் <ssit662@aucklanduni.ac.nz>
2017-07-27 17:33:37 +1200

    work done in labs

--- psoc_intro/lab3.cydsn/main.c
+++ psoc_intro/lab3.cydsn/main.c
@@ -15,12 +15,29 @@
 */
 #include <project.h>
 
+volatile uint8 Flag;
+
+CY_ISR(MyISR) {
+    if (Flag == 0) 
+        Flag = 1;
+    else 
+        Flag = 0;
+    
+    LED_Write(Flag);
+}
+   
+
 int main()
 {
     CyGlobalIntEnable; /* Enable global interrupts. */
+    
+    LED_Write(0);
+    Flag = 0;
+    isr_TC_StartEx(MyISR);
+    Timer_1_Start();
 
     /* Place your initialization/startup code here (e.g. MyInst_Start()) */
-
+    
     for(;;)
     {
         /* Place your application code here. */
